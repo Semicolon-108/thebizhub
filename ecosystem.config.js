08ac7b0ad3b7ef2fab375a7e0dd9c7f590d95303
khue20 <ncomusibsim7@gmail.com>
2023-09-22 13:26:24 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,13 @@
-  module.exports = {
-    apps: [
-      {
-        name: "Guest",
-        script: "serve -s dist -l 9090",
-        args: "start"
-      }
-    ]
-  }; 
+module.exports = {
+  apps: [
+    {
+      name: "Guest",
+      script: "serve -s dist -l 9090",
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
+    }
+  ]
+};
 
-  
