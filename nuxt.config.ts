345ae5d4184929cc564a8aafa24c1074d767213b
khue20 <ncomusibsim7@gmail.com>
2023-09-25 09:11:58 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  ssr: false,
+  ssr: true,
   app: {
     head: {
       link: [{ rel: "icon", type: "image/png", href: "/favicon.ico" }]
