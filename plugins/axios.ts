bfe37e229a114b65889ae1516f2861e1c1c49f59
khue20 <ncomusibsim7@gmail.com>
2023-09-26 16:07:05 +0700

    updated

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-export default defineNuxtPlugin((nuxtApp) => {
-    // const defaultUrl = "http://localhost:4000/client-api/"
-    const defaultUrl = " https://db.thebizhub.la/client-api/"
+export default defineNuxtPlugin((nuxtApp) => { 
+    const defaultUrl = "http://localhost:4000/client-api/"
+    // const defaultUrl = " https://db.thebizhub.la/client-api/"
     axios.defaults.baseURL = defaultUrl
     axios.defaults.headers.common['Authorization'] = localStorage.getItem('bizhubToken') || ''
 
